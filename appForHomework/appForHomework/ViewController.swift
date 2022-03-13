019a4db4071b99bb942d8c6311691a6e50a84d4d
Nikita <telegramomg@ya.ru>
2022-03-13 22:26:41 +0300

    HomeWork done!

---
+++ appForHomework/appForHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appForHomework
+//
+//  Created by Nikita on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
